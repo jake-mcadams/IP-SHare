3461b93a7a4a4d91d39dcfb2a7473aa67a4011a8
jake-mcadams <jake.mcadams@yahoo.com>
2022-05-01 09:58:24 -0400

    added leaflet and openmap

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,14 @@
 "use strict";
+const searchValue = document.getElementById("#search");
+let ipLocation = {
+    lon: 40.7484,
+    lat: 73.9857,
+    zoom: 5
+};
+const map = L.map("map").setView([40.7484, -73.9857], 15);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+// creating map layer
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(map);
+const marker = L.marker([40.7484, -73.9857]).addTo(map);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-const searchValue = document.getElementById('#search') as HTMLInputElement;
+const searchValue = document.getElementById("#search") as HTMLInputElement;
+
+let ipLocation: {lon: number, lat: number, zoom:number} ={
+  lon: 40.7484,
+  lat: 73.9857,
+  zoom: 5
+}
+
+const map = L.map("map").setView([40.7484, -73.9857], 15);
+
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+// creating map layer
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(map);
+
+const marker = L.marker([40.7484, -73.9857]).addTo(map)
