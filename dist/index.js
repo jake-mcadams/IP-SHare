5416ab7bec5b054ce0e65b7566d00f765b248628
McAdams <Jake.McAdams@alvaria.com>
2022-05-11 13:07:30 -0400

    additional fetch logic

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,23 @@
 "use strict";
+// import { marker } from "leaflet";
+// import * as L from "leaflet";
 let searchValue = document.getElementById("search");
 const searchForm = document.getElementById("search_form");
-searchForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    // console.log(getCordinates())
-    getCordinates();
-});
+let browserRDO;
+let myLocation = {
+    ip: "73.106.192.3",
+    isp: "Comcast",
+    location: "Acworth" + "GA" + "30101",
+    timezone: "-04:00",
+    lat: 34.06635,
+    lng: -84.67837,
+};
 // Get IP address co-ordinates
 const getCordinates = () => {
+    let rdo_ip;
     const myHeaders = new Headers({
         "Content-Type": "application/json",
     });
-    // let testURL = `https://geo.ipify.org/api/v2/country,city?apiKey=at_oAlnCfwFErXLt8X82dbK9K5XKSTk4&ipAddress${searchValue.value}`;
     let testURL = `http://localhost:3000/dbTest`;
     const myRequest = new Request("http://localhost:3000/dbTest", {
         method: "POST",
@@ -22,7 +28,7 @@ const getCordinates = () => {
     fetch(myRequest, {
         body: JSON.stringify({
             ip: searchValue.value,
-        })
+        }),
     })
         .then((response) => {
         if (!response.ok) {
@@ -31,29 +37,38 @@ const getCordinates = () => {
         return response.json();
     })
         .then((data) => {
-        // let wantedData: {ip: number, location:any, isp: number} = {
-        //   ip: data["ip"],
-        //   location: data["location"],
-        //   isp: data["isp"]
-        // }
-        // console.log(data["Result"]["geoname_id"])
-        for (let key of Object.keys(data)) {
-            console.log(Object.values(data[key]));
-        }
+        console.log(data);
     });
 };
-let myLocation = {
-    ip: '73.106.192.3',
-    isp: "Comcast",
-    location: "Acworth" + "GA" + "30101",
-    timezone: '-04:00',
-    lat: 34.06635,
-    lng: -84.67837
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let rdoLat;
+    let rdoLng;
+    // console.log(getCordinates());
+    getCordinates();
+    navigator.geolocation.getCurrentPosition(getBrowserLocation);
+    updateLocation(34.06635, -84.67837, 15);
+    console.log(browserRDO);
+});
+const getBrowserLocation = (position) => {
+    // console.log(position.coords.latitude)
+    // console.log(position.coords.longitude)
+    console.log(position.coords);
+    browserRDO = position.coords;
+    return browserRDO;
 };
-const map = L.map("map").setView([myLocation.lat, myLocation.lng], 15);
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 // creating map layer
+// const generateMap = (x: number = 0, y: number = 0, z: number = 1): any => {
+const map = L.map("map").setView([0, 0], 1);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const tiles = L.tileLayer(tileUrl, { attribution });
 tiles.addTo(map);
 const marker = L.marker([myLocation.lat, myLocation.lng]).addTo(map);
+// };
+const updateLocation = (x, y, z) => {
+    map.setView([x, y], z);
+    marker.setLatLng([x, y]);
+};
+//Generating default view
+// generateMap();
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
+// import { marker } from "leaflet";
+// import * as L from "leaflet";
+
 let searchValue = document.getElementById("search") as HTMLInputElement;
 const searchForm = document.getElementById("search_form") as HTMLFormElement;
 
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // console.log(getCordinates())
-  getCordinates();
-  
-});
+let browserRDO:any;
+
+let myLocation: {
+  ip: string;
+  isp: string;
+  location: string;
+  timezone: string;
+  lat: number;
+  lng: number;
+} = {
+  ip: "73.106.192.3",
+  isp: "Comcast",
+  location: "Acworth" + "GA" + "30101",
+  timezone: "-04:00",
+  lat: 34.06635,
+  lng: -84.67837,
+};
 
 // Get IP address co-ordinates
 
-const getCordinates = () => {
+const getCordinates = (): any => {
+  let rdo_ip: string;
   const myHeaders = new Headers({
     "Content-Type": "application/json",
   });
@@ -24,50 +39,65 @@ const getCordinates = () => {
     cache: "default",
   });
 
-
   fetch(myRequest, {
     body: JSON.stringify({
       ip: searchValue.value,
-    })
+    }),
   })
-    .then((response)=>{
-      if(!response.ok) {
-        throw new Error("Network response was not ok")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
       return response.json();
     })
-    .then((data)=>{
-
-    for (let key of Object.keys(data)){
-      console.log(Object.values(data[key]))
-    }
-
-    })
+    .then((data) => {
+      console.log(data)
+    });
 };
 
+searchForm.addEventListener("submit", (e) => {
+  
+  e.preventDefault();
+  let rdoLat:any;
+  let rdoLng: any;
+  // console.log(getCordinates());
+  getCordinates();
+  navigator.geolocation.getCurrentPosition(getBrowserLocation);
+  updateLocation(34.06635, -84.67837, 15);
+  console.log(browserRDO);
+});
 
-  let myLocation: {ip: string, isp: string, location: string, timezone: string, lat:number, lng: number} ={
-    ip: '73.106.192.3',
-    isp: "Comcast",
-    location: "Acworth" + "GA" + "30101",
-    timezone: '-04:00',
-    lat: 34.06635,
-    lng: -84.67837
-  }
-
-
-
-
-const map = L.map("map").setView([myLocation.lat, myLocation.lng], 15);
-
-const attribution =
-  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const getBrowserLocation = (position:any) =>{
+  // console.log(position.coords.latitude)
+  // console.log(position.coords.longitude)
+  console.log(position.coords)
+  browserRDO = position.coords;
+  return browserRDO;
+  
+}
 
 // creating map layer
-const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-const tiles = L.tileLayer(tileUrl, { attribution });
-tiles.addTo(map);
 
-const marker = L.marker([myLocation.lat, myLocation.lng]).addTo(map);
+// const generateMap = (x: number = 0, y: number = 0, z: number = 1): any => {
+  const map = L.map("map").setView([0, 0], 1);
+  
+  const attribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+  const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+  const tiles = L.tileLayer(tileUrl, { attribution });
+  tiles.addTo(map);
+
+  const marker = L.marker([myLocation.lat, myLocation.lng]).addTo(map);
+
+// };
+
+const updateLocation = (x:number, y:number, z:number) =>{
+  map.setView([x, y], z);
+  marker.setLatLng([x, y]);
+}
 
 
+
+//Generating default view
+// generateMap();
